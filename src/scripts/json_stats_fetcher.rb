4c7b480e5564e1a191c2fa8537aa5556d5d766e4
Robey Pointer <robey@twitter.com>
2010-01-05 16:34:58 -0800

    fix usage bug. add -w "web mode" which is compat with thrift-admin.

--- src/scripts/json_stats_fetcher.rb
+++ src/scripts/json_stats_fetcher.rb
@@ -20,12 +20,14 @@ $ganglia_prefix = ''
 stat_timeout = 86400
 hostname = "localhost"
 port = 9989
+use_web = false
 
-def usage
+def usage(port)
   puts
   puts "usage: json_stats_fetcher.rb [options]"
   puts "options:"
   puts "    -n              say what I would report, but don't report it"
+  puts "    -w              use web interface"
   puts "    -h <hostname>   connect to another host (default: localhost)"
   puts "    -p <port>       connect to another port (default: #{port})"
   puts "    -P <prefix>     optional prefix for ganglia names"
@@ -37,13 +39,14 @@ opts = GetoptLong.new(
   [ '-n', GetoptLong::NO_ARGUMENT ],
   [ '-h', GetoptLong::REQUIRED_ARGUMENT ],
   [ '-p', GetoptLong::REQUIRED_ARGUMENT ],
-  [ '-P', GetoptLong::REQUIRED_ARGUMENT ]
+  [ '-P', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '-w', GetoptLong::NO_ARGUMENT ]
   )
 
 opts.each do |opt, arg|
   case opt
   when '--help'
-    usage
+    usage(port)
     exit 0
   when '-n'
     $report_to_ganglia = false
@@ -53,6 +56,9 @@ opts.each do |opt, arg|
     port = arg.to_i
   when '-P'
     $ganglia_prefix = arg
+  when '-w'
+    port = 9990
+    use_web = true
   end
 end
 
@@ -70,7 +76,12 @@ File.open(singleton_file, "w") { |f| f.write("i am running.\n") }
 
 begin
   socket = TCPSocket.new(hostname, port)
-  socket.puts("stats/json#{' reset' if $report_to_ganglia}")
+  if use_web
+    socket.puts("GET /stats#{'/reset' if $report_to_ganglia}")
+    while socket.gets != "\n"; end
+  else
+    socket.puts("stats/json#{' reset' if $report_to_ganglia}")
+  end
   stats = JSON.parse(socket.read)
 
   report_metric("jvm_threads", stats["jvm"]["thread_count"], "threads")
