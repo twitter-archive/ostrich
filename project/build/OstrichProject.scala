b3e888a3b39efa15e3783033f6901a4bb06db812
Robey Pointer <robey@twitter.com>
2010-05-10 15:09:41 -0700

    fix project files for sbt.

---
+++ project/build/OstrichProject.scala
@@ -0,0 +1,19 @@
+import sbt._
+import com.twitter.sbt.StandardProject
+
+
+class OstrichProject(info: ProjectInfo) extends StandardProject(info) {
+  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1"
+  val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
+  val twitterJson = "com.twitter" % "json" % "1.1.2"
+  val configgy = "net.lag" % "configgy" % "1.5.3"
+  val commonsLogging = "commons-logging" % "commons-logging" % "1.1"
+  val commonsLang = "commons-lang" % "commons-lang" % "2.2"
+  val mockito = "org.mockito" % "mockito-core" % "1.8.1"
+  val hamcrest = "org.hamcrest" % "hamcrest-all" % "1.1"
+  val xrayspecs = "com.twitter" % "xrayspecs" % "1.0.7"
+  val cglib = "cglib" % "cglib" % "2.1_3"
+  val asm = "asm" % "asm" % "1.5.3"
+  val objenesis = "org.objenesis" % "objenesis" % "1.1"
+  val netty = "org.jboss.netty" % "netty" % "3.1.5.GA"
+}
