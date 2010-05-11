6816be9a5852b2f0157db4a01b19debd6291d985
Robey Pointer <robey@twitter.com>
2010-05-10 18:30:02 -0700

    allow robey to push.

--- project/build/OstrichProject.scala
+++ project/build/OstrichProject.scala
@@ -16,4 +16,6 @@ class OstrichProject(info: ProjectInfo) extends StandardProject(info) {
   val asm = "asm" % "asm" % "1.5.3"
   val objenesis = "org.objenesis" % "objenesis" % "1.1"
   val netty = "org.jboss.netty" % "netty" % "3.1.5.GA"
+
+  val publishTo = Resolver.sftp("green.lag.net", "green.lag.net", "/web/repo")
 }
