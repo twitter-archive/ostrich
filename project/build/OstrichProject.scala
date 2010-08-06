9535b0cc831cfeb52258542187dbaf7208f78ca0
Jorge Ortiz <jorge.ortiz@gmail.com>
2010-08-06 10:20:48 -0700

    This breaks stuff. Getting rid of it.

--- project/build/OstrichProject.scala
+++ project/build/OstrichProject.scala
@@ -1,8 +1,6 @@
 import sbt._
-import com.twitter.sbt.{SubversionRepository, StandardProject}
 
-
-class OstrichProject(info: ProjectInfo) extends StandardProject(info) with SubversionRepository{
+class OstrichProject(info: ProjectInfo) extends DefaultProject(info) {
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.5"
   // val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
   val twitterJson = "com.twitter" %% "json" % "2.1.3"
