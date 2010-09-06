c0741521253e053f865dc19433bc355af9061273
Robey Pointer <robey@twitter.com>
2010-09-06 12:45:33 -0700

    use newer standard project.

--- project/build/OstrichProject.scala
+++ project/build/OstrichProject.scala
@@ -1,7 +1,6 @@
 import sbt._
 import com.twitter.sbt._
 
-
 class OstrichProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
   val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
