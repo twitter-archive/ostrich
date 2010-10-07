fd34bfdd5e51e8a7ad889c537c11fcd66e784750
Steve Jenson <stevej@fruitless.org>
2010-10-07 14:53:08 -0700

    switch to crossPath resolution.

--- project/build/OstrichProject.scala
+++ project/build/OstrichProject.scala
@@ -3,9 +3,9 @@ import sbt._
 import com.twitter.sbt._
 
 class OstrichProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
-  val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
-  val twitterJson = "com.twitter" % "json" % "2.1.4"
+  val twitterJson = "com.twitter" %% "json" % "2.1.4"
   val configgy = "net.lag" % "configgy" % "2.0.0"
   val commonsLogging = "commons-logging" % "commons-logging" % "1.1"
   val commonsLang = "commons-lang" % "commons-lang" % "2.2"
