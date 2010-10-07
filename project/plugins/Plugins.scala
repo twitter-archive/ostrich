a00f3077a55d876f096860339ee3a10676c683ea
Steve Jenson <stevej@fruitless.org>
2010-10-07 12:26:25 -0700

    update StandardProject to the latest published version.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,5 +3,5 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val scalaTools = "scala-tools.org" at "http://scala-tools.org/repo-releases/"
   val lagNet = "twitter.com" at "http://www.lag.net/repo/"
-  val defaultProject = "com.twitter" % "standard-project" % "0.7.1"
+  val defaultProject = "com.twitter" % "standard-project" % "0.7.4"
 }
