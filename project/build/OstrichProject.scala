dad387f0cf34850a72bf97863c84ba80f1e45b44
Steve Jenson <stevej@twitter.com>
2010-11-12 14:38:47 -0800

    Ostrich is 2.8.1 now.

--- project/build/OstrichProject.scala
+++ project/build/OstrichProject.scala
@@ -3,15 +3,15 @@ import sbt._
 import com.twitter.sbt._
 
 class OstrichProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5"
+  val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
-  val twitterJson = "com.twitter" %% "json" % "2.1.4"
+  val twitterJson = "com.twitter" % "json_2.8.0" % "2.1.4"
   val configgy = "net.lag" % "configgy" % "2.0.0"
   val commonsLogging = "commons-logging" % "commons-logging" % "1.1"
   val commonsLang = "commons-lang" % "commons-lang" % "2.2"
   val mockito = "org.mockito" % "mockito-core" % "1.8.4"
   val hamcrest = "org.hamcrest" % "hamcrest-all" % "1.1"
-  val xrayspecs = "com.twitter" %% "xrayspecs" % "2.0"
+  val xrayspecs = "com.twitter" % "xrayspecs_2.8.0" % "2.0"
   val cglib = "cglib" % "cglib" % "2.1_3"
   val asm = "asm" % "asm" % "1.5.3"
   val objenesis = "org.objenesis" % "objenesis" % "1.1"
