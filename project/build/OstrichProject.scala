5523575dd88fd9b64658a8e5ab1a637eb2aa8697
Kyle Maxwell <kmaxwell@twitter.com>
2010-12-01 11:20:56 -0800

    inlineify project

--- project/build/OstrichProject.scala
+++ project/build/OstrichProject.scala
@@ -1,19 +1,19 @@
 import sbt._
 import com.twitter.sbt._
 
-class OstrichProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
+class OstrichProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher with InlineDependencies {
   val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1"
   val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
-  val twitterJson = "com.twitter" % "json" % "1.1.2"
-  val configgy = "net.lag" % "configgy" % "1.5.5"
+  inline("com.twitter" % "json" % "1.1.7")
+  inline("net.lag" % "configgy" % "[1.6,1.7[")
   val commonsLogging = "commons-logging" % "commons-logging" % "1.1"
   val commonsLang = "commons-lang" % "commons-lang" % "2.2"
-  val mockito = "org.mockito" % "mockito-core" % "1.8.1"
+  val mockito = "org.mockito" % "mockito-core" % "1.8.1" % "test"
   val hamcrest = "org.hamcrest" % "hamcrest-all" % "1.1"
   val xrayspecs = "com.twitter" % "xrayspecs" % "1.0.7"
   val cglib = "cglib" % "cglib" % "2.1_3"
   val asm = "asm" % "asm" % "1.5.3"
-  val objenesis = "org.objenesis" % "objenesis" % "1.1"
+  val objenesis = "org.objenesis" % "objenesis" % "1.1" % "test"
   val netty = "org.jboss.netty" % "netty" % "3.1.5.GA"
 
   override def pomExtra =
