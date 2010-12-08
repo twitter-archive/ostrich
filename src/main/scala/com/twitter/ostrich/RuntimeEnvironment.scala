e45a6f1ffdbb2e87cf3da79e9c0955214b47b6ee
Robey Pointer <robey@twitter.com>
2010-12-08 15:53:16 -0800

    RuntimeEnvironment ported over from configgy.

--- src/main/scala/com/twitter/ostrich/RuntimeEnvironment.scala
+++ src/main/scala/com/twitter/ostrich/RuntimeEnvironment.scala
@@ -1,9 +1,52 @@
+/*
+ * Copyright 2010 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.twitter.ostrich
 
+import java.io.File
 import java.util.Properties
+import scala.collection.mutable
+import net.lag.extensions._
 
+object RuntimeEnvironment {
+  /**
+   * Use the given class to find the `build.properties` file, parse any
+   * command-line arguments, and return the resulting RuntimeEnvironment.
+   */
+  def apply(clazz: Class[_], args: Array[String]) = {
+    val runtime = new RuntimeEnvironment(clazz)
+    runtime.parseArgs(args.toList)
+    runtime
+  }
+}
+
+/**
+ * Use information in a local `build.properties` file to determine runtime
+ * environment info like the package name, version, and installation path.
+ * This can be used to automatically load config files from a `config/` path
+ * relative to the executable jar.
+ *
+ * An example of how to generate a `build.properties` file is included in
+ * sbt stanard-project: <http://github.com/twitter/standard-project>
+ *
+ * You have to pass in a class from your package in order to identify the
+ * location of the `build.properties` file.
+ */
 class RuntimeEnvironment(clazz: Class[_]) {
-  private[this] val buildProperties = new Properties
+  private val buildProperties = new Properties
 
   try {
     buildProperties.load(clazz.getResource("build.properties").openStream)
@@ -15,4 +58,68 @@ class RuntimeEnvironment(clazz: Class[_]) {
   val jarVersion = buildProperties.getProperty("version", "0.0")
   val jarBuild = buildProperties.getProperty("build_name", "unknown")
   val jarBuildRevision = buildProperties.getProperty("build_revision", "unknown")
+  val stageName = System.getProperty("stage", "production")
+
+  /**
+   * Return the path this jar was executed from. Depends on the presence of
+   * a valid `build.properties` file. Will return `None` if it couldn't
+   * figure out the environment.
+   */
+  lazy val jarPath: Option[String] = {
+    val paths = System.getProperty("java.class.path").split(System.getProperty("path.separator"))
+    findCandidateJar(paths, jarName, jarVersion).flatMap { path =>
+      val parent = new File(path).getParentFile
+      if (parent == null) None else Some(parent.getCanonicalPath)
+    }
+  }
+
+  def findCandidateJar(paths: Seq[String], name: String, version: String): Option[String] = {
+    val pattern = ("(.*?)" + name + "(?:_[\\d.]+)?-" + version + "\\.jar$").r
+    paths.find { path =>
+      pattern.findFirstIn(path).isDefined
+    }
+  }
+
+  /**
+   * Config path, as determined from this jar's runtime path, possibly
+   * overridden by a command-line option.
+   */
+  var configPath: File = jarPath match {
+    case Some(path) => new File(path + "/config/" + stageName)
+    case None => new File("/etc/" + jarName)
+  }
+
+  val loggingConfigPath: File = new File(configPath, "logging.scala")
+
+  /**
+   * Perform baseline command-line argument parsing. Responds to `--help`,
+   * `--version`, and `-f` (which overrides the config filename).
+   */
+  def parseArgs(args: List[String]): Unit = {
+    args match {
+      case "-f" :: filename :: xs =>
+        configPath = new File(filename)
+        parseArgs(xs)
+      case "--help" :: xs =>
+        help
+      case "--version" :: xs =>
+        println("%s %s %s %s".format(jarName, jarVersion, jarBuild, jarBuildRevision))
+      case Nil =>
+      case unknown :: _ =>
+        println("Unknown command-line option: " + unknown)
+        help
+    }
+  }
+
+  private def help = {
+    println
+    println("%s %s (%s)".format(jarName, jarVersion, jarBuild))
+    println("options:")
+    println("    -f <path>")
+    println("        path of config files (default: %s)".format(configPath))
+    println("    --version")
+    println("        show version information")
+    println
+    System.exit(0)
+  }
 }
