1892d62db23df6bfa07ef49b34f3e754094b4537
Robey Pointer <robey@twitter.com>
2011-01-18 11:48:52 -0800

    need default repos.

--- project/build/OstrichProject.scala
+++ project/build/OstrichProject.scala
@@ -2,7 +2,7 @@ import sbt._
 
 import com.twitter.sbt._
 
-class OstrichProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
+class OstrichProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher with DefaultRepos {
   override def subversionRepository = Some("http://svn.local.twitter.com/maven-public")
   override def disableCrossPaths = true
   override def managedStyle = ManagedStyle.Maven
