ca76f9351a2bd7d888ffc2eee268fcb78b6e964f
Robey Pointer <robey@twitter.com>
2011-01-23 21:47:50 -0800

    let LogEntry take a StatsCollection in case you don't want to wrap the global one.

--- src/main/scala/com/twitter/ostrich/stats/LogEntry.scala
+++ src/main/scala/com/twitter/ostrich/stats/LogEntry.scala
@@ -26,20 +26,24 @@ import com.twitter.util.Time
 // TODO(benjy): Do we really need four different classes for interaction with a log??
 // (LogEntry, LogReporter, PerThreadStats and StatsLogger!!)
 
-// FIXME: just use a StatsCollection here, not the global Stats.
-
 /**
- * Implementation of a log line. For each request or unit of work, create a new LogEntry instance and
- * use it to track how long work is taking or other metrics about your request. Once you're finished, simply
- * flush the entry.
+ * Implementation of a log line. For each request or unit of work, create a new LogEntry instance
+ * and use it to track how long work is taking or other metrics about your request. Once you're
+ * finished, simply flush the entry.
  *
- * @params logger the Logger to write the entry to
- * @params fields the name of the fields that will be output. If you attempt to write to a column
+ * @param logger the Logger to write the entry to
+ * @param logFormat for generating log lines out of the collected stats
+ * @param fields the name of the fields that will be output. If you attempt to write to a column
  *   that doesn't exist, it will not appear in the log.
+ * @param collection an underlying StatsProvider to copy stats into
  */
-class LogEntry(val logger: Logger, val logFormat: LogFormat, val fields: Array[String]) extends StatsProvider {
+class LogEntry(val logger: Logger, val logFormat: LogFormat, val fields: Array[String],
+               collection: StatsProvider)
+extends StatsProvider {
   val log = Logger.get(getClass.getName)
   val fieldNames: Set[String] = Set.empty ++ fields
+
+  def this(logger: Logger, logFormat: LogFormat, fields: Array[String]) = this(logger, logFormat, fields, Stats)
 
   protected[ostrich] val map: mutable.Map[String, Any] = new mutable.HashMap[String, Any] {
     override def initialSize = fields.length * 2
@@ -55,18 +59,18 @@ class LogEntry(val logger: Logger, val logFormat: LogFormat, val fields: Array[S
 
   def clearAll() = map.clear()
 
-  def getCounter(name: String) = Stats.getCounter(name)
-  def getMetric(name: String) = Stats.getMetric(name)
-  def getGauge(name: String) = Stats.getGauge(name)
-  def getCounters() = Stats.getCounters()
-  def getMetrics() = Stats.getMetrics()
-  def getGauges() = Stats.getGauges()
-  def addGauge(name: String)(gauge: => Double) = Stats.addGauge(name)(gauge)
-  def clearGauge(name: String) = Stats.clearGauge(name)
+  def getCounter(name: String) = collection.getCounter(name)
+  def getMetric(name: String) = collection.getMetric(name)
+  def getGauge(name: String) = collection.getGauge(name)
+  def getCounters() = collection.getCounters()
+  def getMetrics() = collection.getMetrics()
+  def getGauges() = collection.getGauges()
+  def addGauge(name: String)(gauge: => Double) = collection.addGauge(name)(gauge)
+  def clearGauge(name: String) = collection.clearGauge(name)
 
   override def addMetric(name: String, value: Int) {
     log(name, value)
-    Stats.addMetric(name, value)
+    collection.addMetric(name, value)
   }
 
   /**
@@ -74,7 +78,7 @@ class LogEntry(val logger: Logger, val logFormat: LogFormat, val fields: Array[S
    */
   override def addMetric(name: String, distribution: Distribution) {
     // FIXME: can't really log these. TODO(benjy): Revisit this?
-    Stats.addMetric(name, distribution)
+    collection.addMetric(name, distribution)
   }
 
   /**
@@ -108,7 +112,7 @@ class LogEntry(val logger: Logger, val logFormat: LogFormat, val fields: Array[S
 
   override def incr(name: String, count: Int) = {
     log_safe(name, map.getOrElse(name, 0L).asInstanceOf[Long] + count)
-    Stats.incr(name, count)
+    collection.incr(name, count)
   }
 
   /**
