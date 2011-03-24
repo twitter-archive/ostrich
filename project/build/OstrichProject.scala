c91a3743ff3414aae732323194ff1bdb886c22f4
Nick Kallen <nkallen@tallbro.local>
2011-03-23 18:05:55 -0700

    project.version=4.0.2

--- project/build/OstrichProject.scala
+++ project/build/OstrichProject.scala
@@ -2,8 +2,8 @@ import sbt._
 import com.twitter.sbt._
 
 class OstrichProject(info: ProjectInfo) extends StandardLibraryProject(info) with SubversionPublisher with DefaultRepos with IdeaProject {
-  val util = "com.twitter" % "util-core" % "1.8.1"
-  val eval = "com.twitter" % "util-eval" % "1.8.1"
+  val util = "com.twitter" % "util-core" % "1.8.5"
+  val eval = "com.twitter" % "util-eval" % "1.8.5"
   val logging = "com.twitter" % "util-logging" % "1.8.1"
   val json = "com.twitter" % "json_2.8.1" % "2.1.6"
   val scalaCompiler = "org.scala-lang" % "scala-compiler" % "2.8.1" % "compile"
