54416c763ccaf709903806e99edf0d22a1598413
Robey Pointer <robey@twitter.com>
2011-05-05 15:42:57 -0700

    use new standard-project

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -15,7 +15,7 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   }
   override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
 
-  val standardProject = "com.twitter" % "standard-project" % "0.11.16"
+  val standardProject = "com.twitter" % "standard-project" % "0.12.3"
 
   val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
   val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.4.0"
