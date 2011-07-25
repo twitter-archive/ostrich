fea6fb16bdd22e87eda71c3d281186ba5029ec5b
Richard Dallaway <richard@dallaway.com>
2011-07-25 10:36:27 +0100

    Updated dependency for 2.9 version of Specs.

--- project/build/OstrichProject.scala
+++ project/build/OstrichProject.scala
@@ -16,7 +16,7 @@ class OstrichProject(info: ProjectInfo) extends StandardLibraryProject(info)
   val json = "com.twitter" % "json_2.8.1" % "2.1.6"
 
   // for tests:
-  val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test"
+  val specs = "org.scala-tools.testing" % "specs_2.9.0-1" % "1.6.8" % "test"
   val cglib = "cglib" % "cglib" % "2.1_3" % "test"
   val asm = "asm" % "asm" % "1.5.3" % "test"
   val objenesis = "org.objenesis" % "objenesis" % "1.1" % "test"
