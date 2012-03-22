b38e0a2ce886448bc52e223d49462e5fb06f73c6
David Helder <david@twitter.com>
2012-03-22 14:40:15 -0700

    [split] Update to standard-project 1.0.4

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -19,5 +19,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   }
   override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
 
-  val standardProject = "com.twitter" % "standard-project" % "1.0.3"
+  val standardProject = "com.twitter" % "standard-project" % "1.0.4"
 }
