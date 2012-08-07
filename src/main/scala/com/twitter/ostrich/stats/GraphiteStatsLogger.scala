933b97e760e16722db59d4b4e9317857c4e99326
marius a. eriksen <marius@twitter.com>
2012-08-07 16:34:25 -0500

    [split] a few more ostrich diffs Turns out I had merged from the *internal mirror of ostrich github*, not the github one itself.
    RB_ID=79224

--- src/main/scala/com/twitter/ostrich/stats/GraphiteStatsLogger.scala
+++ src/main/scala/com/twitter/ostrich/stats/GraphiteStatsLogger.scala
@@ -66,7 +66,11 @@ class GraphiteStatsLogger(val host: String, val port: Int, val period: Duration,
   val hostname = InetAddress.getLocalHost.getCanonicalHostName
 
   def periodic() {
-    write(new Socket(host, port))
+    try {
+      write(new Socket(host, port))
+    } catch {
+      case e: IOException => logger.error("Error connecting to graphite: %s", e.getMessage)
+    }
   }
 
   def write(sock: Socket) {
@@ -94,19 +98,19 @@ class GraphiteStatsLogger(val host: String, val port: Int, val period: Duration,
             epoch))
         }}
       } catch {
-        case e: IOException => logger.error("Error writing data to graphite", e)
+        case e: IOException => logger.error("Error writing data to graphite: %s", e.getMessage)
       }
 
       writer.flush()
     } catch {
       case e: Exception =>
-        logger.error("Error writing to Graphite: {}", e.getMessage)
+        logger.error("Error writing to Graphite: %s", e.getMessage)
         if (writer != null) {
           try {
             writer.flush()
           } catch {
             case ioe: IOException =>
-              logger.error("Error while flushing writer:", ioe)
+              logger.error("Error while flushing writer: %s", ioe.getMessage)
           }
         }
     } finally {
@@ -114,7 +118,7 @@ class GraphiteStatsLogger(val host: String, val port: Int, val period: Duration,
         try {
           sock.close();
         } catch {
-          case ioe: IOException => logger.error("Error while closing socket:", ioe)
+          case ioe: IOException => logger.error("Error while closing socket: %s", ioe.getMessage)
         }
       }
       writer = null
