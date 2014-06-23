e41f5b9df4af5daf616e9bbda86aa2d6870b954c
Evan Meagher <evm@twitter.com>
2014-06-23 15:09:53 -0700

    [split] bump util to 6.18.1-SNAPSHOT
    RB_ID=391383
    TBR=true
    NO-QUEUE=true

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Tests._
 
 object Ostrich extends Build {
   val libVersion = "9.5.1"
-  val utilVersion = "6.17.0"
+  val utilVersion = "6.18.0"
 
   val sharedSettings = Seq(
     name := "ostrich",
