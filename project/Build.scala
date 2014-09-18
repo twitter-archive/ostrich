f267b01c5d4d023258f14f7b7d1f370218f65cb4
Moses Nakamura <mnakamura@twitter.com>
2014-09-18 05:16:34 +0000

    [split] csl: publish the world
    RB_ID=450951
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Tests._
 
 object Ostrich extends Build {
   val libVersion = "9.5.6"
-  val utilVersion = "6.21.1"
+  val utilVersion = "6.21.2"
 
   val sharedSettings = Seq(
     name := "ostrich",
